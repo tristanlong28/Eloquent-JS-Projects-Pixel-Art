d7d83594202d5e8d4d5569fcff681d4132827a15
Tristan Long <longtw@umich.edu>
2022-12-29 22:19:17 -0500

    Began PixelEditor

--- pixel_art.js
+++ pixel_art.js
@@ -108,3 +108,37 @@ PictureCanvas.prototype.touch = function(startEvent,
     this.dom.addEventListener("touchend", end);
 };
 
+var PixelEditor = class PixelEditor{
+    constructor(state, config){
+      let {tools, controls, dispatch} = config;
+      this.state = state;
+  
+      this.canvas = new PictureCanvas(state.picture, pos => {
+        let tool = tools[this.state.tool];
+        let onMove = tool(pos, this.state, dispatch);
+        if (onMove) return pos => onMove(pos, this.state);
+      });
+      this.controls = controls.map(
+        Control => new Control(state, config));
+      this.dom = elt("div", {}, this.canvas.dom, elt("br"),
+                     ...this.controls.reduce(
+                       (a, c) => a.concat(" ", c.dom), []));
+    }
+    syncState(state) {
+      this.state = state;
+      this.canvas.syncState(state.picture);
+      for (let ctrl of this.controls) ctrl.syncState(state);
+    }
+}
+
+var ToolSelect = class ToolSelect{
+    constructor(state, {tools, dispatch}){
+        this.select = elt("select", {
+            onchange: () => dispatch({tool: this.select.value})
+        }, ...Object.keys(tools).map(name => elt("option", {
+            selected: name == state.tool
+        }, name)));
+        this.dom = elt("label", null, "🖌 Tool: ", this.select);
+    }
+    syncState(state){this.select.value = state.tool;}
+}
