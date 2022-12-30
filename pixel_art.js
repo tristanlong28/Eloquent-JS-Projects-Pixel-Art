7789a67f8c6de926e5c9143a228d773782393a0a
Tristan Long <longtw@umich.edu>
2022-12-29 21:52:31 -0500

    created picture canvas

--- pixel_art.js
+++ pixel_art.js
@@ -34,3 +34,25 @@ function elt(type, props, ...children){
     return dom;
 }
 
+const scale = 10;
+
+class PictureCanvas{
+    constructor(picture, pointerDown){
+        this.dom = elt("canvas", {
+            onmousedown: event => this.mouse(event, pointerDown),
+            ontouchstart: event => this.touch(event, pointerDown)
+        });
+        this.syncState(picture);
+    }
+    syncState(picture){
+        if (this.picture == picture) return;
+        this.picture = picture;
+        drawPicture(this.picture, this.dom, scale);
+    }
+}
+
+function drawPicture(picture, canvas, scale){
+    canvas.width = picture.width * scale;
+    canvas.height = picture.height * scale;
+    let cx = canvas.getContext("2d");
+}
