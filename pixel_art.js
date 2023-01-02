8ad280b378e6cd1dd5b35934664d5d0348280394
Tristan Long <longtw@umich.edu>
2023-01-02 15:35:27 -0500

    Added more functionality

--- pixel_art.js
+++ pixel_art.js
@@ -181,3 +181,100 @@ function rectangle(start, state, dispatch) {
     drawRectangle(start);
     return drawRectangle;
 }
+
+var around = [{dx: -1, dy: 0}, {dx: 1, dy: 0}, 
+                {dx: 0, dy: -1}, {dx: 0, dy: 1}];
+
+function fill({x, y}, state, dispatch) {
+    let targetColor = state.picture.pixel(x, y);
+    let drawn = [{x, y, color: state.color}];
+    for (let done = 0; done < drawn.length; done++) {
+        for (let {dx, dy} of around) {
+            let x = drawn[done].x + dx, y = drawn[done].y + dy;
+            if (x >= 0 && x < state.picture.width &&
+                y >= 0 && y < state.picture.height &&
+                state.picture.pixel(x, y) == targetColor && 
+                !drawn.some(p => p.x == x && p.y == y)) {
+                    drawnpush({x, y, color: state.color});
+            }
+        }
+    }
+    dispatch({picture: state.picture.draw(drawn)});
+}
+
+function pick(pos, state, dispatch) {
+    dispatch({color: state.picture.pixel(pos.x, pos.y)});
+}
+
+var SaveButton = class SaveButton {
+    constructor(state) {
+        this.picture = state.picture;
+        this.dom = elt("button", {
+            onclick: () => this.save()
+        }, "💾 Save");
+    }
+    save() {
+        let canvas = elt("canvas");
+        drawPicture(this.picture, canvas, 1);
+        let link = elt("a", {
+            href: canvas.toDateURL(),
+            download: "pixelart.png"
+        });
+        document.body.appendChild(link);
+        link.click();
+        link.remove();
+    }
+    syncState(state) { this.picture = state.picture; }
+}
+
+var LoadButton = class LoadButton {
+    constructor(_, {dispatch}) {
+        this.dom = elt("button", {
+            onclick: () => startLoad(dispatch)
+        }, "📁 Load");
+    }
+    syncState() {}
+}
+
+function startLoad(dispatch) {
+    let input = elt("input", {
+        type: "file",
+        onchange: () => finishLoad(input.files[0], dispatch)
+    });
+    document.body.appendChild(input);
+    input.click();
+    input.remove();
+}
+
+function finishLoad(file, dispatch) {
+    if (file == null) return;
+    let reader = new FileReader();
+    reader.addEventListener("load", () => {
+        let image = elt("img", {
+            onload: () => dispatch({
+                picture: pictureFromImage(image)
+            }),
+            src: reader.result
+        });
+    });
+    reader.readAsDateURL(file);
+}
+
+function pictureFromImage(image) {
+    let width = Math.min(100, image.width);
+    let height = Math.min(100, image.height);
+    let canvas = elt("canvas", {width, height});
+    let cx = canvas.getContext("2d");
+    cx.drawImage(image, 0, 0);
+    let pixels = [];
+    let {data} = cx.getImage.Data(0, 0, width, height);
+
+    function hex(n) {
+        return n.toString(16), pad.Start(2, "0");
+    }
+    for (let i = 0; i < data.legnth; i+= 4) {
+        let [r, g, b] = data.slice(i, i + 3);
+        pixels.push("#" + hex(r) + hex(g) + hex(b));
+    }
+    return new Picture(width, height, pixels);
+}
