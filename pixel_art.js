54e7cab948c6558b560555fc29935bb6438bd39f
Tristan Long <longtw@umich.edu>
2023-01-02 15:10:49 -0500

    Adding drawing functionality

--- pixel_art.js
+++ pixel_art.js
@@ -142,3 +142,42 @@ var ToolSelect = class ToolSelect{
     }
     syncState(state){this.select.value = state.tool;}
 }
+
+var ColorSelect = class ColorSelect {
+    constructor(state, {dispatch}) {
+        this.input = elt("input", {
+            type: "color",
+            value: state.color,
+            onchange: () => dispatch({color: this.input.value})
+        });
+        this.dom = elt("label", null, "🎨 Color: ", this.input);
+    }
+    syncState(state) { this.input.value = state.color; }
+}
+
+function draw(pos, state, dispatch) {
+    function drawPixel({x, y}, state) {
+        let drawn = {x, y, color: state.color};
+        dispatch({picture: state.picture.draw([drawn])});
+    }
+    drawPixel(pos, state);
+    return drawPixel;
+}
+
+function rectangle(start, state, dispatch) {
+    function drawRectangle(pos) {
+        let xStart = Math.min(start.x, pos.x);
+        let yStart = Math.min(start.y, pos.y);
+        let xEnd = Math.max(start.x, pos.x);
+        let yEnd = Math.max(start.y, pos.y);
+        let drawn = [];
+        for (let y = yStart; y <= yEnd; y++) {
+            for (let x = xStart; x <= xEnd; x++) {
+                drawn.push({x, y, color: state.color});
+            }
+        }
+        dispatch({picture: state.picture.draw(drawn)});
+    }
+    drawRectangle(start);
+    return drawRectangle;
+}
